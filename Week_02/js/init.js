ed65833be7180efe958a6ddeae2ff10b04424167
avaitt <adhvaith.vijay@gmail.com>
2021-04-15 01:04:11 -0700

    lab 2

--- Week_02/js/init.js
+++ Week_02/js/init.js
@@ -1,21 +1,36 @@
-// var can never change and can always change sometimes
-// dont need to change USE const
-// do need to change USE let
-// var day = 8;
-// var name = "Albert";
-
-//  allows us to test our code without things showing up in the webpage
-console.log("Hello Asian Am 191")
 
 // JavaScript const variable declaration
-const map = L.map('map').setView([34.0709, -118.444], 15);
+const map = L.map('map').setView([34.0709, -118.444],2);
+
 
 // Leaflet tile layer, i.e. the base map
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-//JavaScript let variable declaration to create a marker
-let marker = L.marker([34.0709, -118.444]).addTo(map)
-		.bindPopup('Math Sciences 4328 aka the Technology Sandbox<br> is the lab where I work in ')
+// JavaScript let variable declaration to create a marker
+let marker = L.marker([34.0689, -118.445]).addTo(map)
+		.bindPopup('UCLA')
 		.openPopup();
+let marker1 = L.marker([32.7157, -117.1611]).addTo(map)
+		.bindPopup('San Diego')
+		.openPopup();
+let marker2 = L.marker([12.9716, -77.5946]).addTo(map)
+		.bindPopup('Bengaluru')
+		.openPopup();
+
+
+fetch("js/map.geojson")
+		.then(response => {
+			return response.json();
+			})
+	.then(data =>{
+
+			L.geoJSON(data, {
+				style: function (feature) {
+					return {color: feature.properties.color};
+					}
+			}).bindPopup(function (layer) {
+					return layer.feature.properties.Place;
+			}).addTo(map);
+		});
