d27b0e88e539eef9fae67ca93779916c8a1bae9f
avaitt <adhvaith.vijay@gmail.com>
2021-04-15 01:25:03 -0700

    lab 2 updates

--- Week_02/js/init.js
+++ Week_02/js/init.js
@@ -1,19 +1,23 @@
 
+// JavaScript const variable declaration
+const map = L.map('map').setView([34.0709, -118.444],2);
+
+
 // Leaflet tile layer, i.e. the base map
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 // JavaScript let variable declaration to create a marker
-let marker = L.marker([34.0689, -118.445]).addTo(map)
-		.bindPopup('UCLA')
-		.openPopup();
-let marker1 = L.marker([32.7157, -117.1611]).addTo(map)
-		.bindPopup('San Diego')
-		.openPopup();
-let marker2 = L.marker([12.9716, -77.5946]).addTo(map)
-		.bindPopup('Bengaluru')
-		.openPopup();
+// let marker = L.marker([34.0689, -118.445]).addTo(map)
+// 		.bindPopup('UCLA')
+// 		.openPopup();
+// let marker1 = L.marker([32.7157, -117.1611]).addTo(map)
+// 		.bindPopup('San Diego')
+// 		.openPopup();
+// let marker2 = L.marker([12.9716, -77.5946]).addTo(map)
+// 		.bindPopup('Bengaluru')
+// 		.openPopup();
 
 
 fetch("js/map.geojson")
