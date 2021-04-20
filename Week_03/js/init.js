232806d114e349233cc5f67eb3335f08008139b4
avaitt <adhvaith.vijay@gmail.com>
2021-04-20 01:15:11 -0700

    Lab 3

--- Week_03/js/init.js
+++ Week_03/js/init.js
@@ -1,6 +1,6 @@
 // declare variables
-let zoomLevel = 5;
-const mapCenter = [34.0709,-118.444];
+let zoomLevel = 2.45;
+const mapCenter = [40.4637, 3.7492];
 
 // use the variables
 const map = L.map('map').setView(mapCenter, zoomLevel);
@@ -9,31 +9,42 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+var greenIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
+
 // create a function to add markers
 function addMarker(lat,lng,title,message){
     console.log(message)
-    L.marker([lat,lng]).addTo(map).bindPopup(`<h2>${title}</h2>`)
+    L.marker([lat,lng], {icon:greenIcon}).addTo(map).bindPopup(`<h2>${title}</h2>`)
     createButtons(lat,lng,title); // new line!!!
     return message
 }
+
 
 // create a function to add buttons with a fly to command
 function createButtons(lat,lng,title){
     const newButton = document.createElement("button"); // adds a new button
     newButton.id = "button"+title; // gives the button a unique id
+    newButton.src = "http://www.gravatar.com/avatar/bf4cc94221382810233575862875e687?s=150"
     newButton.innerHTML = title; // gives the button a title
     newButton.setAttribute("lat",lat); // sets the latitude 
     newButton.setAttribute("lng",lng); // sets the longitude 
 
     // attach an event listner to the button with Leaflet's map.flyTo
     newButton.addEventListener('click', function(){
-        map.flyTo([lat,lng]); 
+        map.flyTo([lat,lng], 8);
     })
     document.body.appendChild(newButton); //this adds the button to our page.
 }
 
 // use our marker functions
-addMarker(37,-122,'home','home land!')
-addMarker(32,-118,'work','where i work land!')
-addMarker(39,-119,'location 1','random location')
-addMarker(36,-120,'location 2','another random location')
+addMarker(12.9716, 77.5946, 'Bangalore, India','Where My Family Is')
+addMarker(34.0689,-118.4452, 'UCLA','My Favorite Place In The World!')
+addMarker(27.9881, 86.9250, 'Mt. Everest','A Mountain I Want to Climb')
+
